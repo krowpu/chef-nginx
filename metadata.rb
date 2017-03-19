be140fa738408bba97c61c20c3e56a88c0702b71
krowpu <krowpu@tightmail.com>
2017-03-19 14:08:36 +0500

    Add metadata

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,12 @@ name 'nginx'
 description 'Nginx web server'
 license 'MIT'
 
+maintainer 'krowpu'
+maintainer_email 'krowpu@tightmail.com'
+
+source_url 'https://github.com/krowpu/chef-nginx'
+issues_url 'https://github.com/krowpu/chef-nginx/issues'
+
 platform 'ubuntu', '= 16.04'
 
 depends 'dist-update'
